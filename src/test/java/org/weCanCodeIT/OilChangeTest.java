7916a0c8373004c3eb182b4eb1a5a0a6278ed759
Jimmy McCue <xstreamfishn@gmail.com>
2023-03-16 13:01:02 -0400

    created an inherited class

---
+++ src/test/java/org/weCanCodeIT/OilChangeTest.java
@@ -0,0 +1,12 @@
+package org.weCanCodeIT;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OilChangeTest {
+    @Test
+    public void createNewOilChange() {
+        Service oilChange = new OilChange();
+    }
+}
