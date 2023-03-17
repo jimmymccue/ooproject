5a55806ae6ab90829937e8a91fd75f278f7a09b8
Jimmy McCue <xstreamfishn@gmail.com>
2023-03-17 09:30:48 -0400

    added functionality to SyntheticOilChange

--- src/main/java/org/weCanCodeIT/SyntheticOilChange.java
+++ src/main/java/org/weCanCodeIT/SyntheticOilChange.java
@@ -4,4 +4,9 @@ public class SyntheticOilChange extends OilChange {
 
     //Inheritance-- SyntheticOilChange inherits from OilChange and Service.  It has access to the public and
     // protected methods that they have.
+
+
+    public void setCost() {
+        super.cost = 100;
+    }
 }
--- src/test/java/org/weCanCodeIT/SyntheticOilChangeTest.java
+++ src/test/java/org/weCanCodeIT/SyntheticOilChangeTest.java
@@ -2,6 +2,8 @@ package org.weCanCodeIT;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SyntheticOilChangeTest {
 
     @Test
@@ -9,4 +11,12 @@ class SyntheticOilChangeTest {
         ServiceInterface service = new SyntheticOilChange();
     }
 
+    @Test
+    public void testGetPrice() {
+        OilChange oilChange = new SyntheticOilChange();
+        //This code represents abstraction.  oilChange doesn't care how setCost() gets implemented.  It is up to
+        //SyntheticOilChange to take care of these details.
+        oilChange.setCost();
+        assertEquals(100, oilChange.getCost());
+    }
 }
